05b532d88253e7a025e64f48ac431b39b18c7066
viswanathv5741 <League@AdministorsiMac.attlocal.net>
2018-03-17 11:28:16 -0700

    Asdf
    Asdf

--- src/intro_to_file_io/Todolist.java
+++ src/intro_to_file_io/Todolist.java
@@ -3,13 +3,17 @@ package intro_to_file_io;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Todolist implements ActionListener {
 	public static void main(String[] args) {
+		Todolist asdf = new Todolist();
+		
 	}
 
 	JFrame tiny = new JFrame();
@@ -18,9 +22,10 @@ public class Todolist implements ActionListener {
 	JButton load = new JButton();
 	JButton save = new JButton();
 	JButton remove = new JButton();
+	ArrayList<String> task = new ArrayList<String>();
 
 	Todolist() {
-		
+
 		tiny.add(small);
 		small.add(remove);
 		small.add(add);
@@ -31,15 +36,29 @@ public class Todolist implements ActionListener {
 		load.setText("Load Task");
 		save.setText("Save task");
 		tiny.setVisible(true);
-		tiny.setSize(500, 500);
+		tiny.setSize(500, 75);
 		add.addActionListener(this);
 		load.addActionListener(this);
 		remove.addActionListener(this);
 		save.addActionListener(this);
+		add.getMouseListeners();
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-	}
+		if (e.getSource().equals(add)) {
+			String work = JOptionPane.showInputDialog("Do you want to add someting to your to do list?");
+			task.add(work);
+		}
+		if (e.getSource().equals(load)) {
+			JOptionPane.showConfirmDialog(null, "What do you want to do? " + task);
+		}
+		if (e.getSource().equals(remove)) {
+			String not = JOptionPane.showInputDialog("Which task do you want to get rid of" + add);
 
+		}
+		if (e.getSource().equals(remove)) {
+			
+		}
+	}
 }
